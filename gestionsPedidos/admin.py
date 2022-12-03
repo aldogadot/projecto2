2c88a0b32b4f3d2aa9f2c5fab557e5111bab4e76
aldo_ <aldoarreola69@gmail.com>
2022-12-03 13:43:21 -0600

    video 17 y 18

--- gestionsPedidos/admin.py
+++ gestionsPedidos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from gestionsPedidos.models import Clientes,Articulos,Pedidos
 
 # Register your models here.
+class ClientesAdmin(admin.ModelAdmin):
+    list_display = ("nombre","direccion","tfno")
+    search_fields = ("nombre","tfno")
+
+admin.site.register(Clientes,ClientesAdmin)
+admin.site.register(Articulos)
+admin.site.register(Pedidos)
